5104b14b8500b74682c48bdc0f281b7caab00857
KingLud <cossu.luca@gmail.com>
2023-05-20 16:17:51 +0200

    disable ubl fading + lcd timeout to main menu

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1432,7 +1432,7 @@
 
 #if EITHER(HAS_DISPLAY, DWIN_LCD_PROUI)
   // The timeout to return to the status screen from sub-menus
-  //#define LCD_TIMEOUT_TO_STATUS 60000
+  #define LCD_TIMEOUT_TO_STATUS 30000
 
   #if ENABLED(SHOW_BOOTSCREEN)
     #define BOOTSCREEN_TIMEOUT 2000       // (ms) Total Duration to display the boot screen(s)
